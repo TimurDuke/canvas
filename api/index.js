92cb0c20bbc79fb5aaeda1398023c34545d03210
Timur Kim <kimtimur01@gmail.com>
2022-10-07 20:29:39 +0600

    Added actions on the server to handle getting all pixels and creating pixels. (Api)

--- api/index.js
+++ api/index.js
@@ -10,15 +10,36 @@ require('express-ws')(app);
 app.use(cors());
 
 const activeConnections = {};
+let pixels = [];
 
-app.ws("/drawer", (ws, req) => {
+app.ws("/drawer", ws => {
     const id = nanoid();
 
     console.log("Client connected! id = " + id);
     activeConnections[id] = ws;
 
     ws.on("message", msg => {
+        const decodedMessage = JSON.parse(msg);
 
+        switch (decodedMessage.type) {
+            case "GET_ALL_PIXELS":
+                ws.send(JSON.stringify({type: "ALL_PIXELS", pixels}));
+                break;
+            case "CREATE_PIXELS":
+                Object.keys(activeConnections).forEach(connId => {
+                    const conn = activeConnections[connId];
+
+                    pixels = pixels.concat(decodedMessage.newPixels)
+
+                    conn.send(JSON.stringify({
+                        type: "NEW_PIXEL",
+                        newPixels: decodedMessage.newPixels
+                    }));
+                });
+                break;
+            default:
+                console.log("Unknown message type: ", decodedMessage.type);
+        }
     });
 });
 
